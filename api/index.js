9efa13e760a567d5fe8b92b06ac9f5b065e53116
Hazel Phaho <Hazelphaho@gmail.com>
2024-10-16 18:39:50 +0200

    vercel json

--- api/index.js
+++ api/index.js
@@ -17,25 +17,12 @@ const groupMember = require("./model/GroupMember");
 const Invitation = require("./model/Invitation");
 const Transaction = require("./model/Transaction");
 
-// const corsConfig = {
-//   optionsSuccessStatus: 200,
-//   origin: "https://stokfin.vercel.app",
-//   methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
-// };
-// app.use(cors(corsConfig));
-app.use(cors());
-// app.options("*", cors(corsConfig));
-
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "https://stokfin.vercel.app");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   next();
-// });
+const corsConfig = {
+  optionsSuccessStatus: 200,
+  origin: "https://stokfin.vercel.app",
+  methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+};
+app.use(cors(corsConfig));
 
 app.use(bodyParser.json());
 
